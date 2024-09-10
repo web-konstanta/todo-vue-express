806ec8d9648dad068b0016f66ba0bac1a86b313b
Makarenko Kostya <kochanmujik@gmail.com>
2024-09-10 17:17:24 +0300

    Created frontend for creating and updating todo

--- client/src/components/Todo/TodoCreate.vue
+++ client/src/components/Todo/TodoCreate.vue
@@ -1,4 +1,5 @@
 <script>
+import '../../assets/css/form.css'
 import HeaderTemplate from '../Templates/HeaderTemplate'
 
 export default {
@@ -7,8 +8,19 @@ export default {
 </script>
 
 <template>
-    <HeaderTemplate/>
-    <div class="container">
-        <h1>Todo create</h1>
-    </div>
+    <HeaderTemplate />
+    <form class="todo">
+        <h1 class="todo__title">Todo create</h1>
+        <div class="form__item">
+            <label>Title</label>
+            <input class="form__input" type="text">
+        </div>
+        <div class="form__item">
+            <label>Description</label>
+            <textarea class="form__input" type="text"></textarea>
+        </div>
+        <div class="form__item">
+            <button class="form__send">Save</button>
+        </div>
+    </form>
 </template>
